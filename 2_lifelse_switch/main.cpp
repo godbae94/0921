d42eb51ce1e031c235e7e512f101d5cce5d1e275
godbae94 <145646904+godbae94@users.noreply.github.com>
2023-09-22 13:52:52 +0900

    Add files via upload

---
+++ 2_lifelse_switch/main.cpp
@@ -0,0 +1,214 @@
+﻿#include <iostream>
+#include <stdio.h>
+
+int main()
+{
+	{
+		//int a = 0;
+		//std::cin >> a;
+		//std::cout << a << "를 입력하셨습니다.";
+	}
+
+	{
+		// %d : 부호 있는 10진수 정수
+		// %I64d : 부호 있는 10진수 정수(8바이트)
+		// %u : 부호 없는 10진수 정수
+		// %I64u : 부호 없는 10진수 정수(8바이트)
+		// %f : 부호 있는 실수(12.3456789);
+		// %e, %E : 부호 있는 실수(1.23456789e + 001 )
+		// %i : 부호 있는 10진수 정수(10진수/8진수/16진수 입력 가능)
+		// %x : 부호 없는 16진수 정수(0123456789abcdef)
+		// %X : 부호 없는 16진수 정수(0123456789ABCDEF)
+		// %o : 부호 없는 8진수
+		// %p : 포인터 값을 16진수로 표기
+		// %c : 단일 문자 출력
+		
+		//int a = 0, b = 0;
+		//scanf_s ("%d %d", &a, &b);
+		//printf("a더하기 b는? %d.", a + b);
+	}
+
+	//bool bResult = true;
+
+	//if (bResult)
+	//{
+	//	printf("참이다");
+	//}
+	//else
+	//{
+	//	printf("거짓이다");
+	//}
+
+	//int Score = 0;
+	//printf("Score를 입력해주세요:");
+	//scanf_s("%d", &Score);
+
+	////printf("Score = %d", Score);
+
+	//if (Score >= 90)
+	//{
+	//	printf("A학점");
+	//}
+	//else if (Score >= 80)
+	//{
+	//	printf("B학점");
+	//}
+	//else if (Score >= 70)
+	//{
+	//	printf("C학점");
+	//}
+	//else if (Score >= 60)
+	//{
+	//	printf("D학점");
+	//}
+	//else if (Score >= 50)
+	//{
+	//	printf("E학점");
+	//}
+	//else
+	//{
+	//	printf("F학점");
+	//}
+
+//	int backNumber = 0;
+//	scanf_s("%d", &backNumber);
+//
+//	switch (backNumber)
+//	{
+//		case 10 :
+//			//break;
+//	    case 11 :
+// 	    {  
+//			//11일때 실행
+//			printf("포수");
+//	     }
+//		break;//종료
+//
+//		case 9:
+//		{
+//			//11일때 실행
+//			printf("투수");
+//		}
+//		break;
+//
+//		
+//		default :
+//		{
+//			//위에 cast가 모두 아니면 실행
+//			printf("후보");
+//		}
+//		break;
+//	}
+//
+//	return 0;
+
+	int light = 0;
+	// scanf_s로 light 값을 입력
+	// light 가 true(1)이면 "파랑" 출력 (printf)
+	// light 가 false(0)이면 "빨강" 출력 (printf)
+	
+	/*scanf_s("%d", &light);
+	if (light)
+	{
+		printf("파랑");
+	}
+	else
+	{
+		printf("빨강");
+	}*/
+	
+	int nlnput = 0;
+	// scanf_s로 nlnput 값을 입력
+	// nlnput 값이 100보다 크면 "100보다 크다" 출력
+	// nlnput 값이 100보다 작으면 "100보다 작다" 출력
+	
+	/*scanf_s("%d", &nlnput);
+	if (nlnput > 100)
+	{
+		printf("100보다 크다");
+	}
+	else if (nlnput < 100)
+	{
+		printf("100보다 작다");
+	}*/
+
+
+
+     int myScore = 0, otherScore = 0;
+	//sanf_s로 myScore, otherScore 값을 입력
+	//myScore가 otherScore 보다 크면 "3점" 출력
+	//myScore가 otherScore 와 같으면 "1점" 출력
+	//myScore가 otherScore 보다 작으면 "0점" 출력
+
+	/*scanf_s("%d %d", &myScore, &otherScore);
+	if (myScore > otherScore)
+	{
+		printf("3점");
+	}
+	else if (myScore == otherScore)
+	{
+		printf("1점");
+	}
+	else if (myScore < otherScore)
+	{
+		printf("0점");
+	}*/
+
+
+
+
+
+	int nCode = 0;
+	//scanf_s 로 nCode 값을 입력
+	//nCode 가 1이나 3이면 "남자" 출력
+	//nCode 가 2이나 4이면 "여자" 출력
+	
+	/*scanf_s("%d", &nCode);
+
+		if (nCode == 1 or nCode == 3)
+		{
+			printf("남자");
+		}
+		else if (nCode == 2 or nCode == 4)
+		{
+			printf("여자");
+		}*/
+
+
+
+
+
+	int nCode2 = 0;
+	//sanf_s로 nCode2 값을 입력
+	// nCode2 가 1이나 2이면 "2000년 미만 출생" 출력
+	// nCode2 가 3이나 4이면 "2000년 이후 출생" 출력
+	
+	/*scanf_s("%d", &nCode2);
+	switch (nCode2)
+	{
+	case 1:
+	case 2:
+		{
+			printf("2000년 미만 출생");
+		}
+		break;
+
+	case 3:
+	case 4:
+		{
+			printf("2000년 이후 출생");
+		}
+		break;
+
+	default:
+	{
+		printf("2000년생");
+	}
+		break;
+	}*/
+
+	// 비트 연산자
+}
+
+
+	
