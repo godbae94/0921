63ec97ed68acbec24bbb7cc3f5e03201848c1433
K-kihwan <alchemy133@gmail.com>
2023-09-22 15:12:28 +0900

    rfrar

--- 2_lifelse_switch/main.cpp
+++ 2_lifelse_switch/main.cpp
@@ -1,5 +1,23 @@
 ﻿#include <iostream>
 #include <stdio.h>
+#include <bitset>
+
+// c++ 의 컴파일
+// c++에서 사람이 작성한 소스코드를 컴퓨터가 읽을수 있는 코드로 변환하여 실행할수 있는 파일로 변환하는 과정
+// 1. 전처리(Preprocessing) 단계 : #include 와 #define 과 같은 전처리기 매크로들을 처리하는 단계
+// 2. 컴파일(Comfile) 단계 : 각각의 소스 파일들을 어셈블리 명령어로 변한
+// 3. 어셈블(Assemble) 단계 : 어셈블리 코드들을 실제 기계어로 이루어진 목적 코드(Object file)로 변환하는 단계
+// 4. 링킹(Linking) 단계 : 각각의 목적 코드들을 한데 모아서 하나의 실행 파일을 생성하는 단계
+
+
+
+
+// 메모리 영역
+// 1. Code 영역 : 실행할 코드가 저장되어있는 영역(기계어로 존재)
+// 2. Data 영역 : 전역변수, Static 변수들이 저장, 프로세스가 종료될떄 까지 메모리에 저장됨
+// 3. Stack 영역 : 지역변수, 매개변수들이 저장, 함수가 호출되면 생성되고 함수가 종료되면 시스템에 반환
+// 4. Heap 영역 : malloc 또는 new 함수에 의해서 동적으로 할당되는 영역, malloc() 또는 new 연산자로 할당, free() 또는 delete 연산자로 해제
+
 
 int main()
 {
@@ -149,7 +167,6 @@ int main()
 	{
 		printf("1점");
 	}
-	else if (myScore < otherScore)
 	{
 		printf("0점");
 	}*/
@@ -159,6 +176,7 @@ int main()
 
 
 	int nCode = 0;
+	
 	//scanf_s 로 nCode 값을 입력
 	//nCode 가 1이나 3이면 "남자" 출력
 	//nCode 가 2이나 4이면 "여자" 출력
@@ -208,6 +226,58 @@ int main()
 	}*/
 
 	// 비트 연산자
+	// c++ 11버전 부터 숫자 앞에 0b를 붙이면 2진법 표기 가능
+	// 2진법 표기 : 앞에 0b를 붙인다 (0b01011101)
+	// 8진법 표기 : 앞에 0을 붙니다 (0123)
+	// 16진법 표기 : 앞에 0x를 붙인다 (0xFF) (A:10, B:11, C:12, D:13, E:14, F:15)
+	int A = 0b01011101;
+	int B = 0123;
+	int C = 0xFF;
+	printf("2진법(Ob01011101): %d\n", A);
+	printf("8진법(0123) : %d\n", B);
+	printf("16진법(0xFF) : %d\n", C);
+
+	// 비트 연산 기호
+	// 곱(&) : 둘 다 1 이면 1, 아니면0
+	// 합(|) : 둘 중에 하나라도 1이면 1, 아니면 0
+	// XOR(^) : 둘이 같으면 0, 아니면 1
+	// 반전(~) : 0이면 1, 1이면 0
+	char bitNum = 0b01001010;
+	char bitResult = 0;
+	bitResult = bitNum & 0b11010100;
+
+	// 0b01001010
+	//+0b11010100
+	// 0b01000000
+	std::cout << std::bitset<8>(bitResult) << std::endl;//std::endl은 개행(print에 \n과 같다)
+
+	bitResult = bitNum | 0b11010100;
+	std::cout << std::bitset<8>(bitResult) << std::endl;//std::endl은 개행(print에 \n과 같다)
+	// 0b01001010
+	//+0b11010100
+	// 0b11011110
+
+	bitResult = bitNum ^ 0b11010100;
+	std::cout << std::bitset<8>(bitResult) << std::endl;
+	// 0b01001010
+	//+0b11010100
+	// 0b01100001
+	
+	bitResult = ~0b11010100;
+	std::cout << std::bitset<8>(bitResult) << std::endl;
+	
+	//10레벨 달성 보상
+	bool isGetLevel10Reward = false;//-> ture
+	//20레발 달성 보상
+	bool isGetLevel20Reward = false;
+	//30레벨 달성 보상
+	bool isGetLevel30Reward = false;
+	//40레벨 달성 보상
+	bool isGetLevel40Reward = false;
+
+
+
+
 }
 
 
